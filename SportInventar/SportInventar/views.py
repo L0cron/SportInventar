7bdaf333a6299e4727ebba627fc952d09a7795d9
l0cron <l0cron@mail.ru>
2024-12-22 02:07:55 +0300

    Setup visual done

--- SportInventar/SportInventar/views.py
+++ SportInventar/SportInventar/views.py
@@ -1,7 +1,10 @@
-
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
+import ipaddress as ipaddr
 import os
+import psycopg2 as psy
+import re
+from urllib.parse import parse_qs
 def index(request:HttpRequest)->HttpResponse:
     return render(request,'index.html')
 
@@ -11,7 +14,82 @@ def checkInit():
         return True
     return False
 
-def setup(request:HttpRequest)->HttpResponse:
+def setup(request:HttpRequest):
     if checkInit():
         return redirect('index')
-    return render(request,'setup.html')
+    if request.method == 'POST':
+        print(request.POST)
+        status = '0'
+        if 'db' in request.POST:
+            db = parse_qs(request.POST['db'])
+            try:
+                # engine = db['db_engine'][0]
+                name = db['dbname'][0]
+                ip = db['ip'][0]
+                port = db['port'][0]
+                user = db['dbusername'][0]
+                pwd = db['dbpassword'][0]
+                connection_string = f"dbname='{name}' user='{user}' password='{pwd}' host='{ip}' port='{port}'"
+            except:
+                status = 'Присутствуют незаполненные поля'
+            else:
+                try:
+                    connection = psy.connect(connection_string)
+                    status = 'ok'
+                except psy.OperationalError as e:
+                    status = 'Ошибка во время подключения базы: '+str(e)
+                except TimeoutError as e:
+                    status = 'Ошибка, время отведённое на подключение истекло.'
+                finally:
+                    if 'connection' in locals():
+                        connection.close()
+        if 'superuser' in request.POST:
+            user = parse_qs(request.POST['superuser'])
+            try:
+                username = user['username'][0]
+                pwd = user['password'][0]
+                rpwd = user['rpassword'][0]
+                # email = user['email'][0]
+                name = user['name'][0]
+                last_name = user['lastname'][0]
+            except:
+                status = 'Присутствуют незаполненные поля'
+            else:
+                if pwd == rpwd:
+                    if len(pwd) < 12:
+                        status = 'Пароль должен состоять хотя бы из 12 символов'
+                    else:
+                        uname = username.lower()
+                        if not bool(re.match("^[A-Za-z]+$", uname)):
+
+                            status = 'Имя пользователя должно состоять только из букв латинского алфавита'
+                        else:
+                            if len(uname) < 4:
+                                status = 'Имя пользователя должно состоять хотя бы из 4 символов'
+                            else:
+                                status = 'ok'
+                else:
+                    status = 'Пароли не совпадают!'
+        if 'config' in request.POST:
+            config = parse_qs(request.POST['config'])
+            try:
+                servername = config['servername'][0]
+                serverip = config['serverip'][0]
+                serverport = config['serverport'][0]
+            except:
+                status = 'Присутствуют незаполненные поля'
+            else:
+                try:
+                    ipaddr.IPv4Address(serverip)
+                except:
+                    status = 'Некорректный IP адрес сервера'
+                else:
+                    if serverport.isdigit() and int(serverport) > 0 and int(serverport) < 65536:
+                            status = 'ok'
+                    else:
+                        status = 'Некорректный порт сервера'
+                    
+                    
+        return JsonResponse({"status":status})
+    else:
+        return render(request,'setup.html') 
--- SportInventar/static/js/setup.js
+++ SportInventar/static/js/setup.js
@@ -19,6 +19,7 @@ let contents = [content1,content2,content3,content4,finishContent]
 let databaseCheck = false;
 let superuserCheck = false;
 let serverCfgCheck = false;
+let finishCheck = false;
 
 
 function progressStep() {
@@ -60,6 +61,27 @@ function nextStep() {
 
     hideEveryithing(currentStep)
     progressStep();
+    if(currentStep == 2) {
+        if(!superuserCheck) {
+            if(!nextButton.classList.contains('disabled')) {
+                nextButton.classList.add('disabled')
+            }
+        } else {
+            if(nextButton.classList.contains('disabled')) {
+                nextButton.classList.remove('disabled')
+            }
+        }
+    } else if(currentStep == 3) {
+        if(!serverCfgCheck) {
+            if(!nextButton.classList.contains('disabled')) {
+                nextButton.classList.add('disabled')
+            }
+        } else {
+            if(nextButton.classList.contains('disabled')) {
+                nextButton.classList.remove('disabled')
+            }
+        }
+    }
 }
 
 function prevStep() {
@@ -75,5 +97,266 @@ function prevStep() {
     hideEveryithing(currentStep)
 
     progressStep();
+    if(currentStep == 1) {
+        if(!databaseCheck) {
+            if(!nextButton.classList.contains('disabled')) {
+                nextButton.classList.add('disabled')
+            }
+        } else {
+            if(nextButton.classList.contains('disabled')) {
+                nextButton.classList.remove('disabled')
+            }
+        }
+    } else if(currentStep == 2) {
+        if(!superuserCheck) {
+            if(!nextButton.classList.contains('disabled')) {
+                nextButton.classList.add('disabled')
+            }
+        } else {
+            if(nextButton.classList.contains('disabled')) {
+                nextButton.classList.remove('disabled')
+            }
+        }
+    } else if(currentStep == 3) {
+        if(!serverCfgCheck) {
+            if(!nextButton.classList.contains('disabled')) {
+                nextButton.classList.add('disabled')
+            }
+        } else {
+            if(nextButton.classList.contains('disabled')) {
+                nextButton.classList.remove('disabled')
+            }
+        }
+    }
 }
 
+const dbconnect =document.getElementsByName('dbconnect')[0]
+const dbstatus =document.getElementsByName('dbstatus')[0]
+
+
+const superusercheck =document.getElementsByName('superusercheck')[0]
+const superuserstatus =document.getElementsByName('superuserstatus')[0]
+
+const cfgcheck =document.getElementsByName('cfgcheck')[0]
+const cfgstatus =document.getElementsByName('cfgstatus')[0]
+
+const checkcreate =document.getElementsByName('checkcreate')[0]
+
+
+
+const dataurls = document.currentScript.dataset
+const csrftoken = document.getElementsByName('csrfmiddlewaretoken')[0]
+
+$(document).ready(function() {
+    checkcreate.addEventListener('click', function(e) {
+        e.preventDefault();
+        checkcreate.setAttribute('disabled','true')
+        finishCheck = false;
+
+        let status1 = document.getElementById('status1')
+        let status2 = document.getElementById('status2')
+        let status3 = document.getElementById('status3')
+        let status4 = document.getElementById('status4')
+        let status5 = document.getElementById('status5')
+        let status6 = document.getElementById('status6')
+        
+
+        let data1 = {'db':$('#setup-form1').serialize(),
+            'csrfmiddlewaretoken':csrftoken.value
+        }
+
+        $.ajax({
+            method: 'post',
+            url: dataurls['setup_url'],
+            data: data1,
+            success: function(e) {
+                status1.textContent = e.status
+                if(e.status == 'ok') {
+                    status1.style.color = 'green'
+                    // checksPassed+=1;
+                }
+            }
+        })
+
+        let data2 = {'superuser':$('#setup-form2').serialize(),
+            'csrfmiddlewaretoken':csrftoken.value
+        }
+
+        $.ajax({
+            method: 'post',
+            url: dataurls['setup_url'],
+            data: data2,
+            success: function(e) {
+                status2.textContent = e.status
+                if(e.status == 'ok') {
+                    status2.style.color = 'green'
+                    // checksPassed+=1;
+                }
+            }
+        })
+
+        let data3 = {'config':$('#setup-form3').serialize(),
+            'csrfmiddlewaretoken':csrftoken.value
+        }
+
+        $.ajax({
+            method: 'post',
+            url: dataurls['setup_url'],
+            data: data3,
+            success: function(e) {
+                status3.textContent = e.status
+                if(e.status == 'ok') {
+                    status3.style.color = 'green'
+                    // checksPassed+=1;
+                }
+            }
+        })
+        data1['migrate']=1;
+        data2['migrate']=1;
+        data3['migrate']=1;
+        
+        $.ajax({
+            method: 'post',
+            url: dataurls['setup_url'],
+            data: data1,
+            success: function(e) {
+                status4.textContent = e.status
+                if(e.status == 'ok') {
+                    status4.style.color = 'green'
+                    // checksPassed+=1;
+                }
+            }
+        })
+        
+        $.ajax({
+            method: 'post',
+            url: dataurls['setup_url'],
+            data: data2,
+            success: function(e) {
+                status5.textContent = e.status
+                if(e.status == 'ok') {
+                    status5.style.color = 'green'
+                    // checksPassed+=1;
+                }
+            }
+        })
+        $.ajax({
+            method: 'post',
+            url: dataurls['setup_url'],
+            data: data3,
+            success: function(e) {
+                status6.textContent = e.status
+                if(e.status == 'ok') {
+                    status6.style.color = 'green'
+                    // checksPassed+=1;
+                }
+            }
+        })
+
+        finishCheck = true;
+        if(finishButton.classList.contains('disabled')) {
+            finishButton.classList.remove('disabled')
+        }
+        if(!backButton.classList.contains('disabled')) {
+            backButton.classList.add('disabled')
+        }
+
+        
+        // if(checksPassed != 6) {
+        checkcreate.removeAttribute('disabled')
+
+        // }
+        
+    })
+
+    dbconnect.addEventListener('click', function(e) {
+        e.preventDefault();
+        dbconnect.setAttribute('disabled','true')
+        dbstatus.style.color = 'black'
+        dbstatus.textContent = 'Подключение...'
+        databaseCheck = false;
+        if(!nextButton.classList.contains('disabled')) {
+            nextButton.classList.add('disabled')
+        }
+        let data = {'db':$('#setup-form1').serialize(),
+            'csrfmiddlewaretoken':csrftoken.value
+        }
+        
+        $.ajax({
+            method: 'post',
+            url: dataurls['setup_url'],
+            data: data,
+            success: function(e) {
+                dbconnect.removeAttribute('disabled')
+                dbstatus.textContent = e.status
+                if(e.status == 'ok') {
+                    dbstatus.style.color = 'green'
+                    databaseCheck = true;
+                    if(nextButton.classList.contains('disabled')) {
+                        nextButton.classList.remove('disabled')
+                    }
+                }
+            }
+        })
+    })
+
+    superusercheck.addEventListener('click', function(e) {
+        e.preventDefault();
+        superusercheck.setAttribute('disabled','true')
+        superuserstatus.style.color = 'black'
+        superuserstatus.textContent = 'Проверка...'
+        superuserCheck = false;
+        if(!nextButton.classList.contains('disabled')) {
+            nextButton.classList.add('disabled')
+        }
+        let data = {'superuser':$('#setup-form2').serialize(),
+            'csrfmiddlewaretoken':csrftoken.value
+        }
+        $.ajax({
+            method: 'post',
+            url: dataurls['setup_url'],
+            data: data,
+            success: function(e) {
+                superusercheck.removeAttribute('disabled')
+                superuserstatus.textContent = e.status
+                if(e.status == 'ok') {
+                    superuserstatus.style.color = 'green'
+                    superuserCheck = true;
+                    if(nextButton.classList.contains('disabled')) {
+                        nextButton.classList.remove('disabled')
+                    }
+                }
+            }
+        })
+    })
+
+    cfgcheck.addEventListener('click', function(e) {
+        e.preventDefault();
+        cfgcheck.setAttribute('disabled','true')
+        cfgstatus.style.color = 'black'
+        cfgstatus.textContent = 'Проверка...'
+        serverCfgCheck = false;
+        if(!nextButton.classList.contains('disabled')) {
+            nextButton.classList.add('disabled')
+        }
+        let data = {'config':$('#setup-form3').serialize(),
+            'csrfmiddlewaretoken':csrftoken.value
+        }
+        $.ajax({
+            method: 'post',
+            url: dataurls['setup_url'],
+            data: data,
+            success: function(e) {
+                cfgcheck.removeAttribute('disabled')
+                cfgstatus.textContent = e.status
+                if(e.status == 'ok') {
+                    cfgstatus.style.color = 'green'
+                    serverCfgCheck = true;
+                    if(nextButton.classList.contains('disabled')) {
+                        nextButton.classList.remove('disabled')
+                    }
+                }
+            }
+        })
+    })
+})
