8692c420889ab6f69478345ee12d1dd8ac38ff37
Skostya <skostya9255@gmail.com>
2025-01-22 18:20:02 +0300

    some fix

--- SportInventar/procurements/models.py
+++ SportInventar/procurements/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Procurement(models.Model):
-    url = models.CharField(verbose_name="Ссылка",max_length=255,null=True,blank=True)
-    name = models.CharField(verbose_name="Название", max_length=255,null=True,blank=True)
-    price = models.CharField(verbose_name="Цена", max_length=255,null=True,blank=True)
-    amount = models.IntegerField(verbose_name="Количество",null=True,blank=True)
-    supplier = models.CharField(verbose_name="Поставщик", max_length=255,null=True,blank=True)
-    photoPath = models.CharField(verbose_name="Путь к иконке",max_length=255,null=True,blank=True)
+    url = models.CharField(verbose_name="Ссылка",max_length=255,null=True,blank=True,default='')
+    name = models.CharField(verbose_name="Название", max_length=255,null=True,blank=True,default='')
+    price = models.CharField(verbose_name="Цена", max_length=255,null=True,blank=True,default='')
+    amount = models.IntegerField(verbose_name="Количество",null=True,blank=True,default=0)
+    supplier = models.CharField(verbose_name="Поставщик", max_length=255,null=True,blank=True,default='')
+    photoPath = models.CharField(verbose_name="Путь к иконке",max_length=255,null=True,blank=True,default='')
