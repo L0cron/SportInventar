a5cf19951da60ec173356a9237617b341fadb67e
marg0kolbasa <margaritakleymen2007@gmail.com>
2025-01-23 15:59:49 +0300

    fixed design

--- SportInventar/static/js/procurements.js
+++ SportInventar/static/js/procurements.js
@@ -1,13 +1,17 @@
 function searchMain() {
     var searchValue = document.getElementById('search').value;
-    var url = `buy\\${searchValue}`;
-    window.location.href = url;
+    if(searchValue !== ""){
+        var url = `buy\\${searchValue}`;
+        window.location.href = url;
+    }
 }
 
 function search() {
     var searchValue = document.getElementById('search').value;
-    var url = `${searchValue}`;
-    window.location.href = url;
+    if(searchValue !== ""){
+        var url = `${searchValue}`;
+        window.location.href = url;
+    }
 }
 
 function openModal() {
