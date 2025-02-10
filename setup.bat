ef516f01d712083a60d9ff01be18855ab35c715b
l0cron <l0cron@mail.ru>
2025-02-10 18:50:26 +0300

    css minor fix

--- setup.bat
+++ setup.bat
@@ -2,12 +2,12 @@
 chcp 65001
 cls
 title Project Setup
-echo Спортивный инветарь 1.0.0
+@REM echo Спортивный инветарь 1.0.0
 echo.
-echo Установка зависимостей
+@REM echo Установка зависимостей
 python dependencies.py
 echo.
-echo Начало установки
-echo Внимание: Установка проекта возможна только при отсутствии config.json файла в данной директории
+@REM echo Начало установки
+@REM echo Внимание: Установка проекта возможна только при отсутствии config.json файла в данной директории
 python setup.py init
 pause
